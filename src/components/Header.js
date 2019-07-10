01e2d6c723b48f33539acbe2cf33aef3cabbcf25
Soravath Mengseng <soravathmengseng@Soravaths-MacBook-Air.local>
2019-07-10 10:47:41 -0400

    fixed navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,19 +3,22 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return(
-    <div>
-      <div className="toggleNav">
-            <label for="toggle"><i class="fa fa-bars"></i></label>
-      </div>
 
-      <input type="checkbox" id="toggle" name="" value=""/>
-       <div className="navbar">
-            <a><Link to='/'>Experience</Link></a>
-            <a><Link to='/projects'>Projects</Link></a>
-            <a><Link to='education'>Education</Link></a>
-       </div>
+     <div>
+       <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <ul class="navbar-nav ml-auto">
+              <a class="nav-item nav-link"><Link to='/'>Experience</Link></a>
+              <a class="nav-item nav-link"><Link to='/projects'>Projects</Link></a>
+              <a class="nav-item nav-link"><Link to='education'>Education</Link></a>
+            </ul>
+          </div>
+        </nav>
+    </div>
 
-     </div>
   );
 }
 
